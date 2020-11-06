f2ef708f0b8c6914dbcf3b84d18586fd8bcc8322
Alex-Jay <alexjaycontact@gmail.com>
2020-11-06 16:49:36 +0000

    Hook up firestore to project

--- public/js/place.js
+++ public/js/place.js
@@ -2,13 +2,22 @@ const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
-
 const signInBtn = document.getElementById('signInBtn');
 const signOutBtn = document.getElementById('signOutBtn');
 
 const userDetails = document.getElementById('userDetails');
 
 const provider = new firebase.auth.GoogleAuthProvider();
+
+const db = firebase.firestore();
+
+let fakeCounter = 0;
+
+const addColor = document.getElementById('submit');
+const colorList = document.getElementById('colorList');
+
+let colorsRef;
+let unsubscribe;
 
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
@@ -19,11 +28,27 @@ auth.onAuthStateChanged(user => {
     whenSignedIn.hidden = false;
     whenSignedOut.hidden = true;
     userDetails.innerHTML = `<h3>Welcome ${user.displayName}! </h3><p>User ID: ${user.uid}</p>`
+    
+    colorsRef = db.collection('colors');
+
+    addColor.onclick = () => {
+      const { serverTimestamp } = firebase.firestore.FieldValue;
+
+      colorsRef.add({
+        uid: user.uid,
+        name: `user${fakeCounter+1}`,
+        placedAt: serverTimestamp(),
+        isVip: Math.random() >= 0.5
+      });
+    }
+
   } else {
     whenSignedIn.hidden = true;
     whenSignedOut.hidden = false;
   }
 });
+
+
 
 // $(document).ready(() => {
 //   var socket = io();
